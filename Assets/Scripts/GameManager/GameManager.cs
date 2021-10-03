bc1d2f81d23bc74cec85c41001133c5a79ecc2dd
Jolo Tan <jolotan416@gmail.com>
2021-10-03 18:38:21 +0800

    Add UI for instructions

--- Assets/Scripts/GameManager/GameManager.cs
+++ Assets/Scripts/GameManager/GameManager.cs
@@ -1,10 +1,23 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
+    public Text scoreText;
+    public Text instructionsText;
+    public ScoreTracker scoreTracker;
+
+    public string GAME_OVER_TEXT = "Score: ";
+    public string RESTART_INSTRUCTIONS_TEXT = "Press R to restart";
+
     float timeScale = 0f;
     bool isGameEnded = false;
+
+    public void Start()
+    {
+        Invoke("HideInstructions", 3f);
+    }
 
     public void Update()
     {
@@ -18,13 +31,23 @@ public class GameManager : MonoBehaviour
     {
         timeScale = Time.timeScale;
         Time.timeScale = 0f;
+
+        scoreTracker.enabled = false;
+        scoreText.text = GAME_OVER_TEXT + scoreText.text;
+        instructionsText.text = RESTART_INSTRUCTIONS_TEXT;
         isGameEnded = true;
     }
 
     private void Restart()
     {
+        scoreTracker.enabled = true;
         Time.timeScale = timeScale;
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
         isGameEnded = false;
+    }
+
+    private void HideInstructions()
+    {
+        instructionsText.text = "";
     }
 }
